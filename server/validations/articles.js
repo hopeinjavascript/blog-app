fa094921e73ae0b32290b4db5a4eff58a1fc2196
hopeinjavascript <hopeinjavascript@gmail.com>
2023-07-08 16:50:04 +0530

    fix: add validation for required parameters

--- server/validations/articles.js
+++ server/validations/articles.js
@@ -2,9 +2,9 @@ import HTTP_STATUS_CODES from '../constants/httpStatusCodes.js';
 import { throwError } from '../helpers/generic.js';
 
 const createArticle = (req, _, next) => {
-  const { title, content } = req.body;
+  const { title, coverImage, content } = req.body;
 
-  if (!title || !content)
+  if (!title || !coverImage || !content)
     throwError('All fields are required', HTTP_STATUS_CODES.BAD_REQUEST);
 
   next();
@@ -34,7 +34,7 @@ const updateArticle = (req, _, next) => {
     throwError('"action" is required', HTTP_STATUS_CODES.BAD_REQUEST);
 
   if (action === 'edit' && (!title || !coverImage || !content)) {
-    throwError('title and content is required', HTTP_STATUS_CODES.BAD_REQUEST);
+    throwError('All fields are required', HTTP_STATUS_CODES.BAD_REQUEST);
   }
 
   next();
